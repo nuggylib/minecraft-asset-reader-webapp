30d6147b62b4db64846e5d08cd671d2a0abf4809
Armando Vasquez <ar.xa.vasquez@gmail.com>
2021-04-13 00:01:18 -0500

    Consume data from CLI server POC

--- src/ConfigureContentMap.tsx
+++ src/ConfigureContentMap.tsx
@@ -3,20 +3,30 @@ import {
     RawAssetData,
     ConfiguredBlock
 } from "./types"
+import axios from "axios"
 import { ChakraProvider } from "@chakra-ui/react"
 
 export const ConfigContentMap = (props: {
+    namespace?: string
     rawData?: RawAssetData
 }) => {
+
+    const namespace = !!props.namespace ? props.namespace : "minecraft"
+
     const [selectedBlocks, setSelectedBlocks] = useState([] as string[])
     const [configuredBlocks, setConfiguredBlocks] = useState([] as ConfiguredBlock[])
+    const [rawBlocks, setRawBlocks] = useState([] as {}[])
 
     useEffect(() => {
         // TODO: Use axios to get the data from the back end
+        axios.get(`http://localhost:3000/content-map/configure/blocks?namespace=minecraft`)
+            .then(res => {
+                console.log(`RESPONSE: `, res)
+            })
     })
 
-    const namespaces = Object.keys(props.rawData!)
-    const rawBlockNames = Object.keys(props.rawData![namespaces[0]].model.block)
+    // const namespaces = Object.keys(props.rawData!)
+    // const rawBlockNames = Object.keys(props.rawData![namespaces[0]].model.block)
 
     return(
         <div className="container">
