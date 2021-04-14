4e0231fa729906511e5313e645504188bccd4202
Armando Vasquez <ar.xa.vasquez@gmail.com>
2021-04-14 00:59:30 -0500

    Display textures consumed from internal server (separate repo)

--- src/ConfigureContentMap.tsx
+++ src/ConfigureContentMap.tsx
@@ -5,6 +5,7 @@ import {
 } from "./types"
 import axios from "axios"
 import { ChakraProvider } from "@chakra-ui/react"
+import { BlockModelData } from "./minecraft/types"
 
 export const ConfigContentMap = (props: {
     namespace?: string
@@ -15,13 +16,19 @@ export const ConfigContentMap = (props: {
 
     const [selectedBlocks, setSelectedBlocks] = useState([] as string[])
     const [configuredBlocks, setConfiguredBlocks] = useState([] as ConfiguredBlock[])
-    const [rawBlocks, setRawBlocks] = useState([] as {}[])
+    const [blockRecords, setBlockRecords] = useState([] as {
+        block: string
+        data: BlockModelData
+    }[])
 
     useEffect(() => {
         // TODO: Use axios to get the data from the back end
         axios.get(`http://localhost:3000/content-map/configure/blocks?namespace=minecraft`)
             .then(res => {
                 console.log(`RESPONSE: `, res)
+                const blocks = res.data.items
+                console.log(`BLOCKS: `, blocks)
+                setBlockRecords(blocks)
             })
     })
 
@@ -29,7 +36,7 @@ export const ConfigContentMap = (props: {
     // const rawBlockNames = Object.keys(props.rawData![namespaces[0]].model.block)
 
     return(
-        <div className="container">
+        <div className="container mx-auto">
             <header 
                 style={{
                     paddingTop: `2rem`
@@ -37,8 +44,25 @@ export const ConfigContentMap = (props: {
             >
                 <h1>Minecraft Asset Reader</h1>
             </header>
-            <div>
-                
+            <div className="w-full">
+                <ul>
+                    {blockRecords.map(record => {
+                        return (
+                            <li>
+                                {/* Tailwind CSS block as the classname */}
+                                <div className="m-4 p-2 w-full bg-gray-400 bg-opacity-70"> 
+                                    <h3>{record.block}</h3>
+                                    <p>Textures: </p>
+                                    <div className="inline">
+                                        {!!record.data.textures ? 
+                                        Object.keys(record.data.textures).map(textureName => <img src={record.data.textures![textureName]} alt="textureName"/>) 
+                                        : [] }
+                                    </div>
+                                </div>
+                            </li>
+                        )
+                    })}
+                </ul>
             </div>
         </div>
     )
