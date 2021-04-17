7879e3533949576695df162caa87dfebbcc44a7b
ax-vasquez <ar.xa.vasquez@gmail.com>
2021-04-17 13:52:25 -0500

    Disable save button when requisite fields are unset

--- src/BlockModal.tsx
+++ src/BlockModal.tsx
@@ -15,6 +15,7 @@ export const BlockModal = (props: {
     dimiss: () => void
 }) => {
 
+    const [title, setTitle] = useState(``)
     const [top, setTop] = useState(NONE)
     const [left, setLeft] = useState(NONE)
     const [right, setRight] = useState(NONE)
@@ -43,7 +44,29 @@ export const BlockModal = (props: {
     const setTopHandler = (e: any) => setTop(e.target.value)
     const setRightHandler = (e: any) => setRight(e.target.value)
     const setLeftHandler = (e: any) => setLeft(e.target.value)
+    const setTitleHandler = (e: any) => setTitle(e.target.value)
+    const saveHandler = () => {
+        axios.post(`http://localhost:3000/content-map/blocks`, {
+            namespace: props.namespace,
+            blocks: [{
+                [props.blockModelData.block]: {
+                    title,
+                    iconData: {
+                        top,
+                        sideL: left,
+                        sideR: right,
+                    }
+                }
+            }]
+        })
+            .then(res => {
+                console.log(`BLOCK RESPONSE: `, res)
+            })
+            .then(() => props.dimiss())
+    }
 
+    // Saving should be disabled when any of these values are unset
+    const saveButtonDisabled = title.length === 0 || top === NONE || right === NONE || left === NONE
     return (
         <>
             <div className="modal-overlay"/>
@@ -77,7 +100,7 @@ export const BlockModal = (props: {
                     </tbody>
                 </table>
                 <div className="text-center">
-                    <input className="p-1 m-2 bg-gray-300 rounded border-2 border-gray-500 focus:outline-none" type="text" placeholder="Display Name"/>
+                    <input className="modal-input" type="text" placeholder="Title" value={title} onChange={setTitleHandler}/>
                     <div className="modal-dropdown-row">
                         <h3>top</h3>
                         <select value={top} onChange={setTopHandler}>
@@ -103,7 +126,12 @@ export const BlockModal = (props: {
                 <button className="modal-dismiss-button" onClick={props.dimiss}>
                     Dismiss
                 </button>
-                <button className="modal-save-button" onClick={() => console.log(`SAVE!`)}>
+                
+                <button className={
+                    !!saveButtonDisabled ?
+                    "modal-save-button-disabled" :
+                    "modal-save-button"
+                } onClick={saveHandler} disabled={saveButtonDisabled}>
                     Save
                 </button>
             </div>
