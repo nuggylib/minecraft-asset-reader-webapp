c21a9025fed1b0aa1c796dd5ad8c62dce83c7144
ax-vasquez <ar.xa.vasquez@gmail.com>
2021-04-17 15:09:38 -0500

    Cache block config in back end - read from cache for stored data

--- src/BlockModal.tsx
+++ src/BlockModal.tsx
@@ -1,10 +1,65 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useReducer, useState } from 'react'
 import { useScaledBlockImages } from './hooks/useScaledBlockImages'
 import { BlockModelData } from './minecraft/types'
 import axios from "axios"
 import { ConfiguredBlock } from './types'
 
 const NONE = `none`
+
+enum BLOCK_MODAL_ACTION {
+    SET_TITLE = `set_title`,
+    SET_TOP = `set_top`,
+    SET_LEFT = `set_left`,
+    SET_RIGHT = `set_right`,
+    LOAD_CACHED = `load_cached`
+}
+
+/**
+ * Most actions are really straightforward The only "complex" case to handle is the `LOAD_CACHED` 
+ * action, which needs to set all of the cached values simultaenously (which can't be done using 
+ * `useEffect` due to how React batches state change updates)
+ */
+const reducer = (prevState: any, action: any) => {
+    switch (action.type) {
+        case BLOCK_MODAL_ACTION.SET_TITLE: {
+            return  {
+                ...prevState,
+                title: action.payload.title,
+            }
+        }
+        case BLOCK_MODAL_ACTION.SET_TOP: {
+            return {
+                ...prevState,
+                top: action.payload.top,
+            }
+        }
+        case BLOCK_MODAL_ACTION.SET_LEFT: {
+            return {
+                ...prevState,
+                left: action.payload.left,
+            }
+        }
+        case BLOCK_MODAL_ACTION.SET_RIGHT: {
+            return {
+                ...prevState,
+                right: action.payload.right,
+            }
+        }
+        case BLOCK_MODAL_ACTION.LOAD_CACHED: {
+            return {
+                title: action.payload.title,
+                top: action.payload.top,
+                left: action.payload.left,
+                right: action.payload.right,
+            }
+        }
+        default: {
+            return {
+                ...prevState
+            }
+        }
+    }
+}
 
 export const BlockModal = (props: {
     namespace: string
@@ -14,11 +69,12 @@ export const BlockModal = (props: {
     }
     dimiss: () => void
 }) => {
-
-    const [title, setTitle] = useState(``)
-    const [top, setTop] = useState(NONE)
-    const [left, setLeft] = useState(NONE)
-    const [right, setRight] = useState(NONE)
+    const [modalState, dispatch] = useReducer(reducer, {
+        title: ``,
+        top: NONE,
+        left: NONE,
+        right: NONE,
+    })
     
     const blockTextures = useScaledBlockImages({
         namespace: props.namespace,
@@ -35,38 +91,53 @@ export const BlockModal = (props: {
     }
 
     useEffect(() => {
-        axios.get(`http://localhost:3000/content-map/block`)
+        axios.get(`http://localhost:3000/content-map/block?namespace=${props.namespace}&block=${props.blockModelData.block}`)
             .then(res => {
-                console.log(`BLOCK RESPONSE: `, res)
-            })
-    }, [top, left, right])
+                const {
+                    title,
+                    iconData
+                } = res.data
 
-    const setTopHandler = (e: any) => setTop(e.target.value)
-    const setRightHandler = (e: any) => setRight(e.target.value)
-    const setLeftHandler = (e: any) => setLeft(e.target.value)
-    const setTitleHandler = (e: any) => setTitle(e.target.value)
+                if (title && iconData) {
+                    dispatch({ type: BLOCK_MODAL_ACTION.LOAD_CACHED, payload: {
+                        title,
+                        top: iconData.top.replace(`blocks/`, ``),
+                        left: iconData.sideL.replace(`blocks/`, ``),
+                        right: iconData.sideR.replace(`blocks/`, ``)
+                    }})
+                }
+            })
+    }, [
+        modalState.title,
+        modalState.top,
+        modalState.left,
+        modalState.right
+    ])
+
+    const setTopHandler = (e: any) => dispatch({ type: BLOCK_MODAL_ACTION.SET_TOP, payload: { top: e.target.value } })
+    const setRightHandler = (e: any) => dispatch({ type: BLOCK_MODAL_ACTION.SET_RIGHT, payload: { right: e.target.value } })
+    const setLeftHandler = (e: any) => dispatch({ type: BLOCK_MODAL_ACTION.SET_LEFT, payload: { left: e.target.value } })
+    const setTitleHandler = (e: any) => dispatch({ type: BLOCK_MODAL_ACTION.SET_TITLE, payload: { title: e.target.value } })
     const saveHandler = () => {
         axios.post(`http://localhost:3000/content-map/blocks`, {
             namespace: props.namespace,
-            blocks: [{
+            blocks: {
                 [props.blockModelData.block]: {
-                    title,
+                    title: modalState.title,
+                    // TODO: Update the backend so that we don't need to add "blocks" here - should be done server-side (just reduces room for error)
                     iconData: {
-                        top,
-                        sideL: left,
-                        sideR: right,
+                        top: `blocks/${modalState.top}`,
+                        sideL: `blocks/${modalState.left}`,
+                        sideR: `blocks/${modalState.right}`,
                     }
                 }
-            }]
+            }
         })
-            .then(res => {
-                console.log(`BLOCK RESPONSE: `, res)
-            })
             .then(() => props.dimiss())
     }
 
     // Saving should be disabled when any of these values are unset
-    const saveButtonDisabled = title.length === 0 || top === NONE || right === NONE || left === NONE
+    const saveButtonDisabled = modalState.title.length === 0 || modalState.top === NONE || modalState.right === NONE || modalState.left === NONE
     return (
         <>
             <div className="modal-overlay"/>
@@ -100,24 +171,24 @@ export const BlockModal = (props: {
                     </tbody>
                 </table>
                 <div className="text-center">
-                    <input className="modal-input" type="text" placeholder="Title" value={title} onChange={setTitleHandler}/>
+                    <input className="modal-input" type="text" placeholder="Title" value={modalState.title} onChange={setTitleHandler}/>
                     <div className="modal-dropdown-row">
                         <h3>top</h3>
-                        <select value={top} onChange={setTopHandler}>
+                        <select value={modalState.top} onChange={setTopHandler}>
                             <option value="none">none</option>
                             {renderDropdownOptions()}
                         </select>
                     </div>
                     <div className="modal-dropdown-row">
                         <h3>sideL</h3>
-                        <select value={left} onChange={setLeftHandler}>
+                        <select value={modalState.left} onChange={setLeftHandler}>
                             <option value="none">none</option>
                             {renderDropdownOptions()}
                         </select>
                     </div>
                     <div className="modal-dropdown-row">
                         <h3>sideR</h3>
-                        <select value={right} onChange={setRightHandler}>
+                        <select value={modalState.right} onChange={setRightHandler}>
                             <option value="none">none</option>
                             {renderDropdownOptions()}
                         </select>
